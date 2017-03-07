6378cb933fc1b2aa8f4152b3ae2fad160fb30f1e
Michael Kohler <me@michaelkohler.info>
2017-03-07 02:33:02 +0100

    Remove header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from './Header';
 import Overview from './Overview';
 import Footer from './Footer';
 import './scss/app.scss';
@@ -7,7 +6,6 @@ import './scss/app.scss';
 const App = () => {
   return (
     <div>
-      <Header />
       <Overview />
       <Footer />
     </div>
