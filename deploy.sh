ee96d005445d23069742af547c547d0091ce677a
Michael Kohler <me@michaelkohler.info>
2018-01-18 00:31:59 +0100

    Create new key for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 eval "$(ssh-agent -s)"
-openssl aes-256-cbc -K $encrypted_d1b4660665e6_key -iv $encrypted_d1b4660665e6_iv -in travis_deploy_rsa.enc -out travis_deploy_rsa -d
+openssl aes-256-cbc -K $encrypted_5895a3b8c317_key -iv $encrypted_5895a3b8c317_iv -in travis_deploy_rsa.enc -out travis_deploy_rsa -d
 chmod 600 travis_deploy_rsa
 ssh-add travis_deploy_rsa
 ssh-keyscan -H me.michaelkohler.info >> ~/.ssh/known_hosts
