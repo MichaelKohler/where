ea31e63b0948073f56d9f6c87e829c681bb5ca04
Michael Kohler <me@michaelkohler.info>
2018-01-18 00:51:01 +0100

    Travis Deployment: run npm install

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 set -x
+
+SERVER_PATH=/var/www/html/where
+
 eval "$(ssh-agent -s)"
 openssl aes-256-cbc -K $encrypted_5895a3b8c317_key -iv $encrypted_5895a3b8c317_iv -in travis_deploy_rsa.enc -out travis_deploy_rsa -d
 chmod 600 travis_deploy_rsa
 ssh-add travis_deploy_rsa
 ssh-keyscan -H me.michaelkohler.info >> ~/.ssh/known_hosts
 
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist/* travis@me.michaelkohler.info:/var/www/html/where/
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/server/* travis@me.michaelkohler.info:/var/www/html/where/server/
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist/* travis@me.michaelkohler.info:$SERVER_PATH/
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/server/* travis@me.michaelkohler.info:$SERVER_PATH/server/
+
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/package.json travis@me.michaelkohler.info:$SERVER_PATH/server/
+ssh travis@me.michaelkohler.info 'cd /var/www/html/where/server/ && npm install'
