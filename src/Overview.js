14df9dd51bb615a4a597cb8554e33f75fde89370
Michael Kohler <me@michaelkohler.info>
2021-04-13 20:51:01 +0200

    Convert Overview to functional component

--- src/Overview.js
+++ src/Overview.js
@@ -8,76 +8,67 @@ import './scss/overview.scss';
 
 import trips from '../trips.json';
 
-class Overview extends React.Component {
-  render() {
-    const pastTrips = trips.visited.filter((trip) => {
-      const now = new Date();
-      const tripStartDate = new Date(trip.dateFrom);
-      return tripStartDate < now;
+const Overview = () => {
+  const pastTrips = trips.visited.filter((trip) => {
+    const now = new Date();
+    const tripStartDate = new Date(trip.dateFrom);
+    return tripStartDate < now;
+  });
+
+  const countries = pastTrips.map((trip) => trip.country);
+  const allCountries = countries.concat(trips.otherVisited);
+  const uniqueCountries = Array.from(new Set(allCountries));
+  const uniqueDestinations = pastTrips.reduce((acc, trip) => {
+    const existingEntry = acc.find((existing) => existing.destination === trip.destination);
+
+    if (!existingEntry) {
+      acc.push(trip);
+    }
+
+    return acc;
+  }, []);
+
+  const totals = {
+    trips: pastTrips.length + trips.otherVisited.length,
+    uniqueDestinations: uniqueDestinations.length,
+    countries: uniqueCountries.length,
+  };
+
+  let nextTrip = {};
+  const today = new Date();
+  trips.visited
+    .sort((a, b) => {
+      return new Date(b.dateFrom) - new Date(a.dateFrom);
+    })
+    .forEach((trip) => {
+      const startDate = new Date(trip.dateFrom);
+
+      if (startDate > today) {
+        nextTrip = trip;
+      }
     });
 
-    const countries = pastTrips.map((trip) => trip.country);
-    const allCountries = countries.concat(trips.otherVisited);
-    const uniqueCountries = Array.from(new Set(allCountries));
-    const uniqueDestinations = pastTrips.reduce((acc, trip) => {
-      const existingEntry = acc.find((existing) => existing.destination === trip.destination);
-
-      if (!existingEntry) {
-        acc.push(trip);
-      }
-
-      return acc;
-    }, []);
-
-    const totals = {
-      trips: pastTrips.length + trips.otherVisited.length,
-      uniqueDestinations: uniqueDestinations.length,
-      countries: uniqueCountries.length,
-    };
-
-    let nextTrip = {};
-    const today = new Date();
-    trips.visited
-      .sort((a, b) => {
-        return new Date(b.dateFrom) - new Date(a.dateFrom);
-      })
-      .forEach((trip) => {
-        const startDate = new Date(trip.dateFrom);
-
-        if (startDate > today) {
-          nextTrip = trip;
-        }
-      });
-
-    return (
-      <div className="container">
-        {
-          this.loading ? <span>Loading..</span>
-            : (
-              <section>
-                <div className="widget-row">
-                  <NextTripWidget
-                    color="brand"
-                    nextTrip={nextTrip}
-                  />
-                  <TotalWidget
-                    color="brand"
-                    totals={totals}
-                  />
-                </div>
-                <Map
-                  trips={uniqueDestinations}
-                  selector="visitedMap"
-                />
-                <TripTable
-                  trips={trips.visited}
-                />
-              </section>
-            )
-      }
+  return (
+    <div className="container">
+      <div className="widget-row">
+        <NextTripWidget
+          color="brand"
+          nextTrip={nextTrip}
+        />
+        <TotalWidget
+          color="brand"
+          totals={totals}
+        />
       </div>
-    );
-  }
-}
+      <Map
+        trips={uniqueDestinations}
+        selector="visitedMap"
+      />
+      <TripTable
+        trips={trips.visited}
+      />
+    </div>
+  );
+};
 
 export default Overview;
