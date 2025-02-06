07f8831dd28304b86cef793ad045515e119c6fdc
Michael Kohler <me@michaelkohler.info>
2025-02-06 09:35:07 +0100

    Fix font references

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "public/styles.css": "styles.css" });
-  eleventyConfig.addPassthroughCopy({ "public/fonts/*": "fonts/*" });
+  eleventyConfig.addPassthroughCopy({ "public/fonts/*": "fonts" });
 };
